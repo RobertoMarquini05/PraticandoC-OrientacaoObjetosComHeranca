a3c7d500291828597a1f793062afd35ea6ab4d35
Roberto Marquini <marquini.roberto05@gmail.com>
2025-09-09 15:16:19 -0300

    Desafio 03

---
+++ Conta Bancaria - Desafio 3/ContaBancaria.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conta_Bancaria___Desafio_3
+{
+    public class ContaBancaria
+    {
+        public Guid Id { get; set; }
+        public string NumeroConta { get; set; }
+        public double Saldo { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public ContaBancaria(string numeroConta, double saldo)
+        {
+            Id = Guid.NewGuid();
+            NumeroConta = numeroConta;
+            Saldo = saldo;
+            CreatedAt = DateTime.Now;
+        }
+
+        public void Depositar(double valorASerDepositado)
+        {
+            Saldo += valorASerDepositado;
+        }
+    }
+}
---
+++ Conta Bancaria - Desafio 3/Program.cs
@@ -0,0 +1,10 @@
+﻿using Conta_Bancaria___Desafio_3;
+
+ContaBancaria conta = new ContaBancaria("78901-2", 1000.00);
+
+Console.WriteLine($"Conta: {conta.NumeroConta}\nSaldo Atual: {conta.Saldo}");
+conta.Depositar(500.00);
+Console.WriteLine($"Depósito realizado com sucesso! Novo saldo: {conta.Saldo}");
+
+// Apenas para não fechar o console
+Console.ReadKey();
