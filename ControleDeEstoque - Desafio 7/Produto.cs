063def0617db191bfe93568538eff4e9bb95a157
Roberto Marquini <marquini.roberto05@gmail.com>
2025-09-09 15:18:26 -0300

    Desafio 07

---
+++ ControleDeEstoque - Desafio 7/Produto.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleDeEstoque___Desafio_7
+{
+    public class Produto
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }    
+        private int QuantidadeEstoque {  get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public Produto(string nome, int quantidadeEstoque)
+        {
+            Id = Guid.NewGuid();
+            Nome = nome;
+            QuantidadeEstoque = quantidadeEstoque;
+            CreatedAt = DateTime.Now;
+        }
+
+        public void Retirar(int quantidade)
+        {
+            if (QuantidadeEstoque > quantidade)
+            {
+                QuantidadeEstoque = QuantidadeEstoque - quantidade;
+            }
+            else
+            {
+                Console.WriteLine($"Erro: Estoque insuficiente para retirada de {quantidade} unidade(s).");
+            }
+        }
+
+        public void ExibirEstoque()
+        {
+            Console.WriteLine($"Estoque: {QuantidadeEstoque}");
+        }
+    }
+}
---
+++ ControleDeEstoque - Desafio 7/Program.cs
@@ -0,0 +1,8 @@
+﻿using ControleDeEstoque___Desafio_7;
+
+Produto produto = new Produto("Caneta Azul", 20);
+produto.Retirar(5);
+produto.ExibirEstoque();
+produto.Retirar(30);
+
+Console.ReadKey();
