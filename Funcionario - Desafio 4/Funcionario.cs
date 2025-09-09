d9c6bcb1f30924002b79e899e905e9c71eaf5640
Roberto Marquini <marquini.roberto05@gmail.com>
2025-09-09 15:16:53 -0300

    Desafio 04

---
+++ Funcionario - Desafio 4/Funcionario.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funcionario___Desafio_4
+{
+    public class Funcionario
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Cargo { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public Funcionario(string nome, string cargo) 
+        {
+            Id = Guid.NewGuid();
+            Nome = nome;
+            Cargo = cargo;
+            CreatedAt = DateTime.Now;
+        }
+
+        public string Promover(string novoCargo)
+        {
+            if (novoCargo != Cargo)
+            {
+                Cargo = novoCargo;
+                return $"Promoção realizada com sucesso!";
+            }
+            else
+                return "Erro: O novo cargo deve ser diferente do cargo atual.";
+        }
+    }
+}
---
+++ Funcionario - Desafio 4/Program.cs
@@ -0,0 +1,10 @@
+﻿using Funcionario___Desafio_4;
+
+Funcionario funcionario = new Funcionario("Carlos Pereira", "Assistente Administrativo");
+Console.WriteLine(funcionario.Promover("Assistente Administrativo"));
+Console.WriteLine(funcionario.Promover("Analista de Projetos"));
+
+Console.WriteLine($"--- Após promoção ---\nFuncionário: {funcionario.Nome}\nCargo Atual: {funcionario.Cargo}");
+
+// Apenas para não fechar o console
+Console.ReadKey();
