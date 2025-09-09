5cc6f533afd55b736b3f5c14e1ea8970074c493f
Roberto Marquini <marquini.roberto05@gmail.com>
2025-09-09 15:18:01 -0300

    Desafio 06

---
+++ ValidacaoAcessoFilme - Desafio 6/Filme.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidacaoAcessoFilme___Desafio_6
+{
+    public class Filme
+    {
+        public Guid Id { get; set; }
+        public string Titulo { get; set; }
+        public int ClassificacaoEtaria { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public Filme(string titulo, int classificacaoEtaria)
+        {
+            Titulo = titulo;
+            ClassificacaoEtaria = classificacaoEtaria;
+        }
+
+        public bool PodeAssistir(int idadeUsuario)
+        {
+            return idadeUsuario >= ClassificacaoEtaria ? true : false;
+        }
+
+        public void ExibirResultado(int idadeUsuario)
+        {
+            string resultado = PodeAssistir(idadeUsuario) ? $"Usuário com {idadeUsuario} anos pode assistir ao filme {Titulo}" : $"Usuário com {idadeUsuario} anos não pode assistir ao filme {Titulo}";
+            Console.WriteLine(resultado);
+        }
+    }
+}
---
+++ ValidacaoAcessoFilme - Desafio 6/Program.cs
@@ -0,0 +1,7 @@
+﻿using ValidacaoAcessoFilme___Desafio_6;
+
+Filme filme = new Filme("Matrix", 16);
+filme.ExibirResultado(14);
+filme.ExibirResultado(20);
+
+Console.ReadKey();
