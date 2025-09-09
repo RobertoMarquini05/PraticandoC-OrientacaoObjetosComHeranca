3d1d554abf969c8e587e26826f09e9d525ae0fb5
Roberto Marquini <marquini.roberto05@gmail.com>
2025-09-09 15:17:35 -0300

    Desafio 05

---
+++ PassagemDeViagem - Desafio 5/Passagem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassagemDeViagem
+{
+    public class Passagem
+    {
+        public Guid Id { get; set; }
+        public string NomePassageiro { get; set; }
+        public string Destino { get; set; }
+        public DateTime DateTime { get; set; }
+
+        public Passagem(string nomePassageiro, string destino)
+        {
+            Id = Guid.NewGuid();
+            NomePassageiro = nomePassageiro;
+            Destino = destino;
+            DateTime = DateTime.Now;
+        }
+
+    }
+}
---
+++ PassagemDeViagem - Desafio 5/Program.cs
@@ -0,0 +1,8 @@
+﻿using PassagemDeViagem;
+
+Passagem passagem = new Passagem("Lucas Souza", "Paris");
+
+Console.WriteLine($"Passageiro: {passagem.NomePassageiro}\nDestino: {passagem.Destino}");
+
+// Apenas para não fechar o console
+Console.ReadKey();
