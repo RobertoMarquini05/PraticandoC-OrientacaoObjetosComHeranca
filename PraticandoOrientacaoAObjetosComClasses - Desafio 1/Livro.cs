3581a592bad38d15197e03287f99fa6fb8215a1a
Roberto Marquini <marquini.roberto05@gmail.com>
2025-09-09 15:15:21 -0300

    Desafio 01

---
+++ PraticandoOrientacaoAObjetosComClasses - Desafio 1/Livro.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PraticandoOrientacaoAObjetosComClasses
+{
+    public class Livro
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }   
+        public string Autor {  get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public Livro(string nome, string autor)
+        {
+            Id = Guid.NewGuid();
+            Nome = nome;
+            Autor = autor;
+            CreatedAt = DateTime.Now;
+        }
+    }
+}
---
+++ PraticandoOrientacaoAObjetosComClasses - Desafio 1/Program.cs
@@ -0,0 +1,9 @@
+﻿using PraticandoOrientacaoAObjetosComClasses;
+using System.Text;
+
+Livro livro = new Livro("Dom Casmurro", "Machado de Assis");
+
+Console.WriteLine($"Livro: {livro.Nome}\nAutor: {livro.Autor}");
+
+// Apenas para não fechar o console
+Console.ReadKey();
